a5e78360f2c72584edad63d224940b302d9b4ecb
vsteidlef <vsteidlef@uc.cl>
2025-04-25 22:27:05 -0400

    improve router for private routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,26 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Inicial from './pages/Inicial';
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import Inicial from "./pages/Inicial";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import LandingPage from "./pages/Landing";
+import { useAuth } from "./context/AuthContext";
 
-import Login from './pages/Login';
-import Register from './pages/Register';
-import LandingPage from './pages/Landing';
-import { useAuth } from './context/AuthContext';
-
-const PrivateRoute = ({ children }) => {
+const PrivateWrapper = () => {
   const { isAuthenticated } = useAuth();
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
-function Router(){
-  const { isAuthenticated } = useAuth();
+function Router() {
   return (
-      <Routes>
-        <Route path={"/"} element={<LandingPage/>}/>
-        <Route path={"/stocks"} element={<Inicial/>}/>
-        <Route path={"/Login"} element={<Login/>}/>
-        <Route path={"/register"} element={<Register/>}/>
-      </Routes>
-  )
+    <Routes>
+      <Route path={"/"} element={<LandingPage />} />
+      <Route path={"/Login"} element={<Login />} />
+      <Route path={"/register"} element={<Register />} />
+      <Route path={"/stocks"} element={<Inicial />} />
+      <Route element={<PrivateWrapper />}>
+        {/* Rutas privadas van aquí */}
+        {/* <Route path={"/stocks"} element={<Inicial />} /> */}
+      </Route>
+    </Routes>
+  );
 }
 
 export default Router;
